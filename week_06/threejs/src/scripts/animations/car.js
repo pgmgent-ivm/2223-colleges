f0de86dab5a945ab57308b67ee948d4d24a5fc14
adriaan-glibert-development <info@adriaanglibert.be>
2023-05-26 11:29:14 +0200

    💄 3D Lights and Simple GSAP Animation

--- week_06/threejs/src/scripts/animations/car.js
+++ week_06/threejs/src/scripts/animations/car.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import gsap from 'gsap';
 
 const $scene = document.getElementById('scene');
 
@@ -19,19 +20,40 @@ $scene.appendChild($canvas);
 const cameraAspectRatio = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(40, cameraAspectRatio, 1, 5000);
 
-camera.position.x = 200;
-camera.position.y = 200;
-camera.position.z = 2000;
+camera.position.x = -200;
+camera.position.y = 50;
+camera.position.z = 1750;
 
 // Setup light
-const ambientLight = new THREE.AmbientLight(0x404040, 100);
+const ambientLight = new THREE.AmbientLight (0x404040,100);
 scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffcd87,30);
+directionalLight.position.set(0,1,0);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+
+const light = new THREE.PointLight(0xfff,10);
+light.position.set(0,300,500);
+scene.add(light);
+
+const light2 = new THREE.PointLight(0xfff,10);
+light2.position.set(500,100,0);
+scene.add(light2);
+
+const light3 = new THREE.PointLight(0x000b82,10);
+light3.position.set(0,100,-500);
+scene.add(light3);
+
+const light4 = new THREE.PointLight(0x000b82,10);
+light4.position.set(-500,300,500);
+scene.add(light4);
 
 // Adding a model to the scene
 const loader = new GLTFLoader();
 loader.load('/car/scene.gltf', (gltf) => {
     const model = gltf.scene.children[0];
-    model.scale.set(1,1,1);
+    model.scale.set(.5,.5,.5);
     scene.add(gltf.scene);
 
     const animate = () => {
@@ -40,4 +62,8 @@ loader.load('/car/scene.gltf', (gltf) => {
     }
 
     animate();
+
+    gsap.to(camera.position, {
+        z: 1000
+    });
 });
