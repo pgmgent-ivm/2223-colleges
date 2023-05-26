fe600cc9f39e5e8db9caf2c23894b137b08973b7
adriaan-glibert-development <info@adriaanglibert.be>
2023-05-26 14:55:24 +0200

    ✨  Dynamic 3D Animations

--- week_06/threejs/src/scripts/animations/car.js
+++ week_06/threejs/src/scripts/animations/car.js
@@ -6,29 +6,36 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 const $scene = document.getElementById('scene');
-const $trigger = document.querySelector("[data-animation='three-d']");
+const $triggers = document.querySelectorAll("[data-animation='three-d']");
 
-// Setup scene
+const X_DEFAULT = 850;
+const Y_DEFAULT = 50;
+const Z_DEFAULT = -2000;
+
+//#region Setup scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color();
+//#endregion
 
-// Setup renderer
+//#region Setup renderer
 const renderer = new THREE.WebGLRenderer({
     antialias: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 const $canvas = renderer.domElement;
 $scene.appendChild($canvas);
+//#endregion
 
-// Setup camera
+//#region Setup camera
 const cameraAspectRatio = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(40, cameraAspectRatio, 1, 5000);
 
-camera.position.x = -200;
-camera.position.y = 50;
-camera.position.z = 1750;
+camera.position.x = 0;
+camera.position.y = 100;
+camera.position.z = 2000;
+//#endregion
 
-// Setup light
+//#region Setup light
 const ambientLight = new THREE.AmbientLight (0x404040,100);
 scene.add(ambientLight);
 
@@ -52,12 +59,18 @@ scene.add(light3);
 const light4 = new THREE.PointLight(0x000b82,10);
 light4.position.set(-500,300,500);
 scene.add(light4);
+//#endregion
 
-// Adding a model to the scene
+//#region Adding a model to the scene
 const loader = new GLTFLoader();
 loader.load('/car/scene.gltf', (gltf) => {
+    //#region Adding model
     const model = gltf.scene.children[0];
-    model.scale.set(.5,.5,.5);
+    model.scale.set(1,1,1);
+    model.position.x = convertNumber(X_DEFAULT, false);
+    model.position.y = Y_DEFAULT;
+    model.position.z = Z_DEFAULT;
+
     scene.add(gltf.scene);
 
     const animate = () => {
@@ -66,25 +79,43 @@ loader.load('/car/scene.gltf', (gltf) => {
     }
 
     animate();
+    //#endregion
 
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: $trigger,
-            scrub: true,
-            end: "bottom bottom",
-            markers: true
-        }
-    });
+    modelAnimation(model);
+});
+//#endregion
 
-    tl.to(camera.position, {
-        x: -500,
-        y: 350,
-        z: 2400,
+const animation = (model, timeline, xPos, yPos, zPos, modelRotation) => {
+    timeline.to(model.position, {
+        x: xPos,
+        y: yPos,
+        z: zPos,
         duration: 2,
         ease: 'power4.inOut'
     }, 0).to(model.rotation, {
-        z: 2,
+        z: modelRotation,
         duration: 2,
         ease: 'power4.inOut'
     }, 0);
-});
+}
+
+const convertNumber = (number, isOdd) => {
+    return isOdd ? -Math.abs(number) : Math.abs(number);
+}
+
+const modelAnimation = (model) => {
+    $triggers.forEach(($trigger, index) => {
+        const isOdd = index % 2;
+        const {x = convertNumber(X_DEFAULT, isOdd), y = convertNumber(Y_DEFAULT, isOdd), z = -1500, rotation = convertNumber(2, isOdd)} = $trigger.dataset;
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: $trigger,
+                scrub: true,
+                end: "bottom bottom"
+            }
+        });
+
+        animation(model, tl, x, y, z, rotation);
+    });
+}
